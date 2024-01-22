b567c27ea29b9591fb4ffd3b2f0ab0644b7b50d6
Guilherme <reiwg1@gmail.com>
2024-01-22 20:57:16 -0300

    Project Base

---
+++ src/main/java/Estacionamento/estacionamentovirtual/EstacionamentoVirtualApplication.java
@@ -0,0 +1,13 @@
+package Estacionamento.estacionamentovirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentoVirtualApplication.class, args);
+	}
+
+}
